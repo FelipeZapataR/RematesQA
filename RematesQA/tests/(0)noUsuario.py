e03984380d1b2595045f23811a2b3050d8c4ce91
FelipeZapataR <62186984+FelipeZapataR@users.noreply.github.com>
2024-01-30 15:51:30 -0300

    Proyecto funcional con el nuevo modelos

---
+++ RematesQA/tests/(0)noUsuario.py
@@ -0,0 +1,31 @@
+from pylenium.driver import Pylenium
+from utilities import Sele
+import time
+import pytest
+
+@pytest.fixture
+def page(py: Pylenium):
+    return Sele(py).goto()  
+
+
+def test_preguntasFrecuentes(page:Sele):
+    page.Click('//*[@id="pageContainer"]/main/section[1]/div/div[1]/div/div/button')
+    assert page.VerifyText('//*[@id="pageContainer"]/main/section/div[1]/h1','Términos y Privacidad')
+    assert page.VerifyText('//*[@id="pageContainer"]/main/section/div[2]/h3[1]','¿Por qué se solicita determinada información para la creación de la cuenta?')
+    page.Click('//*[@id="pageContainer"]/main/section/div[1]/div/button[1]')
+    assert page.VerifyText('//*[@id="pageContainer"]/main/section/div[2]/h3[1]','¿Cómo puedo participar en remates y hacer pujas?')
+    page.Click('//*[@id="navWrapper"]/ul/li[1]/button')
+    assert page.VerifyText('//*[@id="pageContainer"]/main/section[1]/div/div[1]/h1','GyC Remates')
+    page.Click('//*[@id="navWrapper"]/ul/li[3]/button')
+    assert page.VerifyText('//*[@id="pageContainer"]/main/section/div[2]/h3[1]','¿Por qué se solicita determinada información para la creación de la cuenta?')
+
+
+def test_registrarseRemateSinUsuario(page:Sele):
+   page.Click('//*[@id="navWrapper"]/ul/li[2]/button')
+   page.Click('//*[@id="pageContainer"]/main/section/div/div/div[1]/div/div[2]/button[2]')
+   assert page.VerifyText('//*[@id="loginSection"]/div[1]/div[1]/h3','Inicio de sesión')
+
+
+
+
+
---
+++ RematesQA/tests/(4)interaccionRemate.py
@@ -0,0 +1,121 @@
+import time
+from pylenium.driver import Pylenium
+from utilities import Sele
+import pytest
+     
+@pytest.fixture
+def page(py: Pylenium):
+    return Sele(py).goto()   
+
+    
+def test_solicitar_registrarse(page:Sele,py:Pylenium):
+    page.login_usuario()
+    page.Click('/html/body/div[1]/div/div/nav/div[2]/ul/li[2]/button')
+    page.Click('//*[@id="pageContainer"]/main/section/div/div/div[1]/div/div[2]/button[2]')
+    page.Click('//*[@id="modalPortal"]/div/div/div[3]/button')
+    page.Send('//*[@id="garantia-form"]/div[1]/input','Felipe Zapata')
+    page.Send('//*[@id="garantia-form"]/div[2]/input','18.300.115-9')
+    page.Send('//*[@id="garantia-form"]/div[3]/input','Banco Santander')
+    py.getx('//*[@id="garantia-form"]/div[4]/div/select').select_by_text('Cuenta corriente')
+    page.Send('//*[@id="garantia-form"]/div[5]/input','00769696969')
+    page.Click('//*[@id="modalPortal"]/div/div/div[3]/button')
+    time.sleep(2)
+    
+def test_aceptar_garantia(page:Sele):
+    page.login_administrador()
+    page.Click('//*[@id="pageContainer"]/main/section/div/button[3]')
+    page.Click('//*[@id="pageContainer"]/main/section/div/div/div[3]/button[1]')
+    assert page.VerifyText('//*[@id="modalPortal"]/div/div/div[2]/section/div/p',"¿Estás seguro/a que deseas autorizar la solicitud de garantía del usuario felipezapatarodriguez@gmail.com?")
+    page.Click('//*[@id="modalPortal"]/div/div/div[3]/button[2]')
+    time.sleep(2)
+    #mensaje de exito errado
+
+def test_solicitar_registrarse2(page:Sele,py:Pylenium):
+    page.login_usuario2()
+    page.Click('/html/body/div[1]/div/div/nav/div[2]/ul/li[2]/button')
+    page.Click('//*[@id="pageContainer"]/main/section/div/div/div[1]/div/div[2]/button[2]')
+    page.Click('//*[@id="modalPortal"]/div/div/div[3]/button')
+    page.Send('//*[@id="garantia-form"]/div[1]/input','Felipe Zapata')
+    page.Send('//*[@id="garantia-form"]/div[2]/input','18.300.115-9')
+    page.Send('//*[@id="garantia-form"]/div[3]/input','Banco Santander')
+    py.getx('//*[@id="garantia-form"]/div[4]/div/select').select_by_text('Cuenta corriente')
+    page.Send('//*[@id="garantia-form"]/div[5]/input','00769696969')
+    page.Click('//*[@id="modalPortal"]/div/div/div[3]/button')
+    time.sleep(2)
+
+def test_aceptar_garantia2(page:Sele):
+    page.login_administrador()
+    page.Click('//*[@id="pageContainer"]/main/section/div/button[3]')
+    page.Click('//*[@id="pageContainer"]/main/section/div/div/div[3]/button[1]')
+    assert page.VerifyText('//*[@id="modalPortal"]/div/div/div[2]/section/div/p',"¿Estás seguro/a que deseas autorizar la solicitud de garantía del usuario felepex@outlook.es?")
+    page.Click('//*[@id="modalPortal"]/div/div/div[3]/button[2]')
+    time.sleep(2)
+    #mensaje de exito errado
+    
+def test_ver_articulos(page:Sele):
+    page.login_usuario()
+    page.Click('//*[@id="navWrapper"]/ul/li[2]/button')
+    page.Click('//*[@id="pageContainer"]/main/section/div/div/div[1]/div/div[2]/button[1]')
+    page.Click('//*[@id="modalPortal"]/div/div/div[3]/button')
+    page.Click('//*[@id="pageContainer"]/main/section[2]/div/div/div/div/div[1]/div/div[2]/div/div/button')
+    time.sleep(4)
+    
+def test_pujar_articulos(page:Sele):
+    page.login_usuario()
+    #presionar en remates navbar
+    page.Click('//*[@id="navWrapper"]/ul/li[2]/button')
+    #presionar en conectar al remate
+    page.Click('//*[@id="pageContainer"]/main/section/div/div/div[1]/div/div[2]/button[2]')
+    #cerrar card 1
+    page.Click('//*[@id="pageContainer"]/main/section/div/div[1]/button')
+    time.sleep(1)
+    #cerrar card 2
+    page.Click('//*[@id="pageContainer"]/main/section/div/div[2]/button')
+    time.sleep(1)
+    #abrir card 1
+    page.Click('//*[@id="pageContainer"]/main/section/div/div[1]/button')
+    time.sleep(1)
+    #abrir card 2
+    page.Click('//*[@id="pageContainer"]/main/section/div/div[2]/button')
+    time.sleep(1)
+    #pujar el primer articulo por el presion normal
+    page.Click('//*[@id="pageContainer"]/main/section/div/div[1]/div/div[1]/div[3]/div[1]/button[1]')
+    time.sleep(2)
+    #presionar en editar valor del segundio articulo
+    page.Click('//*[@id="pageContainer"]/main/section/div/div[2]/div/div[1]/div[3]/div[1]/button[2]')
+    #limpiar celda de nuevo valoer
+    page.Clear('//*[@id="monto-puja-form"]/div/input')
+    #ingresar valor de nuevo monto a poujar
+    page.Send('//*[@id="monto-puja-form"]/div/input','9000')
+    #presionar aceptar valor
+    page.Click('//*[@id="modalPortal"]/div/div/div[3]/button')
+    #pujar por segundo articulo por nuevo valor
+    page.Click('//*[@id="pageContainer"]/main/section/div/div[2]/div/div[1]/div[3]/div[1]/button[1]')
+    time.sleep(2)
+
+def test_pujar_articulos2(page:Sele):
+    page.login_usuario2()
+    page.Click('//*[@id="navWrapper"]/ul/li[2]/button')
+    #presionar en conectar al remate
+    page.Click('//*[@id="pageContainer"]/main/section/div/div/div[1]/div/div[2]/button[2]')
+    #pujar el primer articulo por el presion normal
+    page.Click('//*[@id="pageContainer"]/main/section/div/div[1]/div/div[1]/div[3]/div[1]/button[1]')
+    time.sleep(2)
+    #presionar en editar valor del segundio articulo
+    page.Click('//*[@id="pageContainer"]/main/section/div/div[2]/div/div[1]/div[3]/div[1]/button[2]')
+    #limpiar celda de nuevo valoer
+    page.Clear('//*[@id="monto-puja-form"]/div/input')
+    #ingresar valor de nuevo monto a poujar
+    page.Send('//*[@id="monto-puja-form"]/div/input','9000')
+    #presionar aceptar valor
+    page.Click('//*[@id="modalPortal"]/div/div/div[3]/button')
+    #pujar por segundo articulo por nuevo valor
+    page.Click('//*[@id="pageContainer"]/main/section/div/div[2]/div/div[1]/div[3]/div[1]/button[1]')
+    time.sleep(2)
+
+          
+    
+    
+    
+        
+    
